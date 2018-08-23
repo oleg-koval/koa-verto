b5ba8d48da68e8294216868410b8763f8b1f9bc8
Oleg Koval <kvl.olg@gmail.com>
2018-08-23 16:47:19 +0200

    fix(functions): Move ternary to fn getValidHeaderValue

--- test/koaIntegration.test.js
+++ test/koaIntegration.test.js
@@ -1,15 +1,14 @@
 const Koa = require('koa');
-const axios = require('axios');
 const setVersionHeaders = require('..');
 
 const { name, version } = require('../package.json');
 
-const verify = (headers) => {
-  expect(headers['x-service-name']).to.be.equal(name);
-  expect(headers['x-service-version']).to.be.equal(version);
+const verify = (headers, options) => {
+  expect(headers['x-service-name']).to.be.equal(options.name);
+  expect(headers['x-service-version']).to.be.equal(options.version);
 };
 
-describe('Should work with Koa as middleware', () => {
+describe('Should work with Koa as middleware with values from package.json', () => {
   let app;
   const url = 'http://localhost:3000/';
 
@@ -24,37 +23,91 @@ describe('Should work with Koa as middleware', () => {
   });
 
   it('has headers for GET request', async () => {
-    const response = await axios.get(url);
-    verify(response.headers);
+    const response = await request.get(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for POST request', async () => {
-    const response = await axios.get(url);
-    verify(response.headers);
+    const response = await request.get(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for PUT request', async () => {
-    const response = await axios.put(url);
-    verify(response.headers);
+    const response = await request.put(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for PATCH request', async () => {
-    const response = await axios.patch(url);
-    verify(response.headers);
+    const response = await request.patch(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for HEAD request', async () => {
-    const response = await axios.head(url);
-    verify(response.headers);
+    const response = await request.head(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for OPTIONS request', async () => {
-    const response = await axios.options(url);
-    verify(response.headers);
+    const response = await request.options(url);
+    verify(response.headers, { name, version });
   });
 
   it('has headers for DELETE request', async () => {
-    const response = await axios.delete(url);
-    verify(response.headers);
+    const response = await request.delete(url);
+    verify(response.headers, { name, version });
+  });
+});
+
+describe('Should work with Koa as middleware with custom values', () => {
+  let app;
+  const url = 'http://localhost:3001/';
+  const options = {
+    name: 'app',
+    version: 'v0.1.1'
+  };
+
+  before((done) => {
+    app = new Koa();
+    app.use(setVersionHeaders(options));
+    app.use((ctx) => {
+      ctx.body = 'ok';
+    });
+    app.listen(3001);
+    done();
+  });
+
+  it('has headers for GET request', async () => {
+    const response = await request.get(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for POST request', async () => {
+    const response = await request.get(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for PUT request', async () => {
+    const response = await request.put(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for PATCH request', async () => {
+    const response = await request.patch(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for HEAD request', async () => {
+    const response = await request.head(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for OPTIONS request', async () => {
+    const response = await request.options(url);
+    verify(response.headers, options);
+  });
+
+  it('has headers for DELETE request', async () => {
+    const response = await request.delete(url);
+    verify(response.headers, options);
   });
 });
